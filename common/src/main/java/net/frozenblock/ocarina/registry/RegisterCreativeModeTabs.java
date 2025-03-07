ce4226408391f87aafe30ddbb6aea0794b489fb7
LiukRast <darthlucavader@gmail.com>
2025-03-07 08:19:03 +0100

    Item group and lang

--- common/src/main/java/net/frozenblock/ocarina/registry/RegisterCreativeModeTabs.java
+++ common/src/main/java/net/frozenblock/ocarina/registry/RegisterCreativeModeTabs.java
@@ -1,14 +1,23 @@
 package net.frozenblock.ocarina.registry;
 
 import net.frozenblock.ocarina.OOMConstants;
+import net.frozenblock.ocarina.platform.OOMServices;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 
 public class RegisterCreativeModeTabs {
-    public static final CreativeModeTab TAB = CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
-            .title(Component.translatable(""))
+    public static final ResourceKey<CreativeModeTab> TAB_KEY = ResourceKey.create(BuiltInRegistries.CREATIVE_MODE_TAB.key(), OOMConstants.id("ocarina_of_mine"));
+    public static final CreativeModeTab TAB = OOMServices.PLATFORM.getCreativeTabBuilder()
+            .icon(() -> new ItemStack(RegisterItems.ZELDAS_OCARINA))
+            .title(Component.translatable("itemGroup.ocarina_of_mine"))
+            .displayItems((params, output) -> {
+                output.accept(RegisterItems.OCARINA);
+                output.accept(RegisterItems.ZELDAS_OCARINA);
+            })
             .build();
 
 
